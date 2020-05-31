0d9329b2d562f05a52997cabbcab7f3b9be64365
Dhaval Shah <dhaval@alphastack.in>
2020-05-31 18:31:19 +0530

    [+] Added Helper functions in BaseViewHolder

--- superadapter/src/main/java/com/alphastack/superadapter/viewholder/BaseViewHolder.kt
+++ superadapter/src/main/java/com/alphastack/superadapter/viewholder/BaseViewHolder.kt
@@ -1,9 +1,38 @@
 package com.alphastack.superadapter.viewholder
 
+import android.content.Context
 import android.view.View
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.alphastack.superadapter.model.RecyclerViewItem
 
 abstract class BaseViewHolder<ItemType : RecyclerViewItem>(view: View) : RecyclerView.ViewHolder(view) {
+
+    /**
+     *
+     */
+    protected fun getContext(): Context = itemView.context
+
+    /**
+     *
+     */
+    protected fun getString(@StringRes resId: Int) = getContext().getString(resId)
+
+    /**
+     *
+     */
+    protected fun getColor(@ColorRes resId: Int) = ContextCompat.getColor(getContext(), resId)
+
+    /**
+     *
+     */
+    protected fun getDrawable(@DrawableRes resId: Int) = ContextCompat.getDrawable(getContext(), resId)
+
+    /**
+     *
+     */
     abstract fun bind(item: ItemType)
 }
